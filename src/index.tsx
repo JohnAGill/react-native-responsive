86e4c47f9b5380a1fac359c96cbe7a0a2998926b
John Gill <john.gill@xdesign.com>
2023-06-21 15:37:17 +0100

    feat: add initial functions for width and height

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,26 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
+import { Dimensions } from 'react-native';
+
+const standardWidth = 430;
+const standardHeight = 932;
+export function responsiveWidth(value: number): number {
+  const dimensions = Dimensions.get('window');
+  const widthDifference = standardWidth - dimensions.width;
+  if (widthDifference !== 0) {
+    const differenceAsPercentage = widthDifference / dimensions.width;
+    const round = value * differenceAsPercentage;
+    const newValue = value - round;
+    return newValue;
+  }
+  return value;
+}
+export function responsiveHeight(value: number): number {
+  const dimensions = Dimensions.get('window');
+  const heightDifference = standardHeight - dimensions.height;
+  if (heightDifference !== 0) {
+    const differenceAsPercentage = heightDifference / dimensions.height;
+    const round = value * differenceAsPercentage;
+    const newValue = value - round;
+    return newValue;
+  }
+  return value;
 }
