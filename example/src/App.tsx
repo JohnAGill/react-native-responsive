c64ddbe8d3148ed1de68d1d65a79a105fd857761
John Gill <john.gill@xdesign.com>
2023-06-21 18:14:57 +0100

    feat: included a hook to parse and return a new stylesheet with updated height width and fontsize

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,69 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-responsive';
+import { StyleSheet, View, ScrollView, SafeAreaView, Text } from 'react-native';
+import { useResponsive } from 'react-native-responsive';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
-
+  const newSS = useResponsive(styles);
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
+    <SafeAreaView style={newSS.container}>
+      <ScrollView>
+        <View style={newSS.box}>
+          <View style={newSS.line1} />
+          <View>
+            <View style={{ flexDirection: 'row' }}>
+              <Text style={newSS.text}>Hello i am some text</Text>
+              <View style={newSS.box2} />
+              <Text style={newSS.text}>Hello i am some text</Text>
+            </View>
+            <Text style={newSS.text}>Hello i am some text</Text>
+
+            <View style={newSS.box2} />
+          </View>
+          <View>
+            <View style={{ flexDirection: 'row' }}>
+              <Text style={newSS.text}>Hello i am some text</Text>
+
+              <View style={newSS.box2} />
+              <Text style={newSS.text}>Hello i am some text</Text>
+            </View>
+            <Text style={newSS.text}>Hello i am some text</Text>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    borderTopWidth: 1,
+    borderTopColor: 'red',
   },
   box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+    width: 400,
+    height: 300,
+    backgroundColor: 'white',
+    marginBottom: 12,
+  },
+  box2: { height: 50, width: 100, backgroundColor: 'pink' },
+  text: { fontSize: 16 },
+  line1: {
+    width: 400,
+    height: 100,
+    backgroundColor: 'red',
+  },
+  line2: {
+    width: 300,
+    height: 100,
+    backgroundColor: 'green',
+  },
+  line3: {
+    width: 200,
+    height: 100,
+    backgroundColor: 'blue',
   },
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 import { Dimensions } from 'react-native';
-
+import _ from 'lodash';
+interface StyleObject {
+  [key: string]: string | number;
+}
+interface StyleSheetType {
+  [key: string]: StyleObject;
+}
 const standardWidth = 430;
 const standardHeight = 932;
 export function responsiveWidth(value: number): number {
@@ -7,6 +13,11 @@ export function responsiveWidth(value: number): number {
   const widthDifference = standardWidth - dimensions.width;
   if (widthDifference !== 0) {
     const differenceAsPercentage = widthDifference / dimensions.width;
+    if (value < 200 && widthDifference > 50) {
+      const round = value * differenceAsPercentage * 2;
+      const newValue = value - round;
+      return newValue;
+    }
     const round = value * differenceAsPercentage;
     const newValue = value - round;
     return newValue;
@@ -23,4 +34,45 @@ export function responsiveHeight(value: number): number {
     return newValue;
   }
   return value;
+}
+
+export function responsiveFontSize(value: number): number {
+  const dimensions = Dimensions.get('window');
+  const heightDifference = standardHeight - dimensions.height;
+  if (heightDifference !== 0) {
+    const differenceAsPercentage = heightDifference / dimensions.height;
+    if (heightDifference > 150) {
+      const round = (value * differenceAsPercentage) / 3;
+
+      const newValue = value - round;
+
+      return newValue;
+    }
+    const round = value * differenceAsPercentage;
+
+    const newValue = value - round;
+
+    return newValue;
+  }
+  return value;
+}
+
+export function useResponsive(styleSheet: StyleSheetType) {
+  const clone = _.cloneDeep(styleSheet);
+  for (const styleClass in clone) {
+    const styleToCheck = clone[styleClass];
+    //check is number
+    // check its there
+    if (styleToCheck?.width && typeof styleToCheck?.width === 'number') {
+      clone[styleClass].width = responsiveWidth(styleToCheck.width);
+    }
+    if (styleToCheck?.height && typeof styleToCheck?.height === 'number') {
+      clone[styleClass].height = responsiveHeight(styleToCheck.height);
+    }
+    if (styleToCheck?.fontSize && typeof styleToCheck?.fontSize === 'number') {
+      clone[styleClass].fontSize = responsiveFontSize(styleToCheck.fontSize);
+    }
+  }
+  console.log('styleSheet: ', styleSheet.text);
+  return clone;
 }
